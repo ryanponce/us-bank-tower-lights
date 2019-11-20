cd6a7dbf56688ea9273ed6dbbff5726f63be8a81
Ryan Ponce <ryan@ponce.la>
2019-11-19 21:45:47 -0800

    Removes image from manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,8 +39,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#000`,
         theme_color: `#000`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`
+        display: `minimal-ui`
       }
     },
     `gatsby-plugin-styled-components`,
