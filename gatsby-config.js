b28cf50a5364c4691626ca59a3f122b093dc0204
Ryan Ponce <ryan@ponce.la>
2019-11-19 22:18:55 -0800

    Remove filesystem plugin for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,6 @@ module.exports = {
         ]
       }
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
